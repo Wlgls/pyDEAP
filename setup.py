e72578dc2e150127a07c7d07f961292ece74d6cd
wlgls <smithguazi@gmail.com>
2021-11-23 10:28:36 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+ 
+import setuptools
+
+setuptools.setup(
+        name="pydeap",
+        version="1.0",
+        author="wlgls",
+        author_email="smithguazi@gmail.com",
+        description="Python for deap dataset",
+        url="https://github.com/wlgls/pyDEAP",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+
+)
